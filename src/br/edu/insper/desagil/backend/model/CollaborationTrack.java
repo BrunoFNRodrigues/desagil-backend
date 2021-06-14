fd902144b84b0debefeab12375cd51eca4bd9f26
BrunoFNRodrigues <bf17669@gmail.com>
2021-06-14 16:24:42 -0300

    Refatora uma a variavel
    colaboradores para colalborators

--- src/br/edu/insper/desagil/backend/model/CollaborationTrack.java
+++ src/br/edu/insper/desagil/backend/model/CollaborationTrack.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CollaborationTrack extends Track{
-	private List<Artist> colaboradores;
+	private List<Artist> colalborators;
 
-	public CollaborationTrack(Artist artist, String name, int duration, List<Artist> colaboradores) {
+	public CollaborationTrack(Artist artist, String name, int duration, List<Artist> colalborators) {
 		super(artist, name, duration);
-		this.colaboradores = colaboradores;
+		this.colalborators = colalborators;
 	}
 	
 	public String getFullArtistName() {
 		List<String> lista = new ArrayList<>();
-		for (Artist artista: colaboradores) {
+		for (Artist artista: colalborators) {
 			lista.add(artista.getName());
 		}
 		return getArtist().getName()+" (feat. "+String.join(", ", lista)+")";
